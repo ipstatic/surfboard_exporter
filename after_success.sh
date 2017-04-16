7ebe60dfe1b91293b7484785786d18c41731efa6
Jarod Watkins <jwatkins@jarodw.com>
2017-04-16 12:08:43 -0400

    Removing Travis CI integration

--- after_success.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir build
-for build in $(find .build -type f); do
-  IFS='/' read -ra path <<< "$build"
-  arch=${path[1]}
-  mv "${build}" "build/surfboard-exporter-${TRAVIS_TAG}.${arch}"
-done
